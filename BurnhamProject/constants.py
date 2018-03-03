aa0738839ed2121531b04e31a1fc86e02e7a2392
alexburn17 <pburnham@uvm.edu>
2018-03-03 14:19:05 -0500

    included all files required to run last homework

---
+++ BurnhamProject/constants.py
@@ -0,0 +1,17 @@
+# length of leg
+L = 0.1
+
+# radius is length divided by 5
+R = L/5
+
+# length of video
+evalTime = 400
+
+# size of population
+popSize = 20
+
+# number of generations
+numGens = 50
+
+# number of envirnoments
+numEnvs = 4
